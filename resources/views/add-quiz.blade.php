cdefc6553bcd11f0fad65608994f443a20922921
anujsaxena2604 <anujsaxena2604@protonmail.com>
2025-08-17 01:27:30 +0530

    Add Quiz

---
+++ resources/views/add-quiz.blade.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Add Quiz Page</title>
+    @vite('resources/css/app.css')
+</head>
+<body>
+    <x-navbar  name={{$name}} ></x-navbar>
+
+    <div class="bg-gray-100 flex flex-col items-center pt-5"> 
+    <div class="bg-white p-8 rounded-2xl shadow-lg w-full max-w-md">
+        @if(!session('quizDetails'))
+        <h2 class="text-2xl text-center text-gray-800 mb-6">Add Quiz</h2>
+        <form action="/add-quiz" method="get" class="space-y-4">
+            
+            <div>
+             <input type="text" placeholder="Enter Quiz name"  name="quiz" 
+                    class="w-full px-4 py-2 border border-gray-300 rounded-xl focus:outline-none focus:ring-2 focus:ring-blue-400">
+            </div>
+
+            <div>
+             <select type="text" name="category_id" 
+                    class="w-full px-4 py-2 border border-gray-300 rounded-xl focus:outline-none focus:ring-2 focus:ring-blue-400">
+                    @foreach ($categories as $category)
+                    <option value={{ $category->id}}>{{ $category->name }}</option>
+                                            
+                    @endforeach
+             </select>
+            </div>
+
+            <button type="submit"
+                class="w-full bg-blue-500 rounded-xl px-4 py-2 text-white hover:bg-blue-600">Add</button>
+
+
+    </form>
+    @else
+    <span class="text-green-500 font-bold">Quiz : {{ session('quizDetails')->name }}</span>
+    <h2 class="text-2xl text-center text-gray-800 mb-6">Add MCQ`s</h2>
+    <form action="" method="get" class="space-y-4">
+    <div>
+        <textarea type="text" placeholder="Enter Your Question"  name="quiz" 
+            class="w-full px-4 py-2 border border-gray-300 rounded-xl focus:outline-none focus:ring-2 focus:ring-blue-400"></textarea>
+    </div>
+    <div>
+        <input type="text" placeholder="Enter First Option"  name="quiz" 
+            class="w-full px-4 py-2 border border-gray-300 rounded-xl focus:outline-none focus:ring-2 focus:ring-blue-400">
+    </div>
+        <div>
+        <input type="text" placeholder="Enter Second Option"  name="quiz" 
+            class="w-full px-4 py-2 border border-gray-300 rounded-xl focus:outline-none focus:ring-2 focus:ring-blue-400">
+    </div>
+        <div>
+        <input type="text" placeholder="Enter Third Option"  name="quiz" 
+            class="w-full px-4 py-2 border border-gray-300 rounded-xl focus:outline-none focus:ring-2 focus:ring-blue-400">
+    </div>
+        <div>
+        <select name="right answer" 
+            class="w-full px-4 py-2 border border-gray-300 rounded-xl focus:outline-none focus:ring-2 focus:ring-blue-400">
+            <option>Select Right Answer</option>
+            <option>A</option>
+            <option>B</option>
+            <option>C</option>
+            <option>D</option>
+        </select>
+    </div>
+    <button type="submit"
+                class="w-full bg-blue-500 rounded-xl px-4 py-2 text-white hover:bg-blue-600">Add More</button>
+        <button type="submit"
+                class="w-full bg-green-500 rounded-xl px-4 py-2 text-white hover:bg-blue-600">Add Submit</button>
+</form>            
+@endif
+</div>
+</div>
+</body>
+</html>
